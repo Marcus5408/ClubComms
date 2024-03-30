595b9eff598e0ba753bcac7ea74d68b9f5a9fce4
Marcus Jones <marcusjones8119@gmail.com>
2024-03-30 17:21:34 -0400

    mv from main repo

--- clubcomms/__init__.py
+++ clubcomms/__init__.py
@@ -2,10 +2,21 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return flask.render_template('clubcomms.html')
+    return flask.render_template("clubcomms.html")
 
 
-if __name__ == '__main__':
+@app.route("/demo/")
+def demo():
+    return flask.render_template("demo.html", url=flask.url_for('static', filename='demo'))
+
+
+@app.route("/demo/<path:path>")
+def media(path):
+    return flask.send_from_directory(app.static_folder, path)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
